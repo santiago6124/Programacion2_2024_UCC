2c230ca36587b62ade0474cb3c0af3e4f0ab190a
santiago6124 <santiagocarranzazinny@gmail.com>
2024-05-14 16:53:10 -0300

    task insided main

--- POO/EJ reloj/main.cpp
+++ POO/EJ reloj/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include "Reloj.h"
 using namespace std;
+/*
+Se desea implementar un software que represente un reloj como el de la figura:
+Se pide implementar los objetos Hora y Temperatura
+Requerimientos Hora:
+1) Se debe permitir poner en hora el reloj ( Set Hora).
+1) La hora se representa en el formato de 00:00:00 a 23:59:59
+1) Configurar Alarma ( Set Alarma).
+1) Cuando la alarma se active, la misma debe durar un determinado tiempo (Ej: 20 seg)
+Requerimientos Temperatura
+1) El reloj toma temperatura ambiente a través de un sensor. (Se simula simplemente seteando por
+teclado una temperatura inicial de trabajo)
+2) Se puede mostrar la temperatura en 3 escalas distintas.
+*/
+
 
 int main()
 {
